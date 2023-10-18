b8573144b9b8fa711f9cea629826e7607baf7640
Robert Fleming <robertefleming.ca@gmail.com>
2023-10-18 09:56:30 -0700

    Initial Commit

---
+++ CalcuLuxe/CalcuLuxeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcuLuxeApp.swift
+//  CalcuLuxe
+//
+//  Created by Robert Edward Fleming on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CalcuLuxeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalcuLuxe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalcuLuxe
+//
+//  Created by Robert Edward Fleming on 10/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
